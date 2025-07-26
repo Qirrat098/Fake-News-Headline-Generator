a0a74f86741d321aa189d6dc90dcc1c9d2b36766
Qirrat098 <qirratazam098@gmail.com>
2025-07-27 00:52:40 +0500

    initial commit

---
+++ code.py
@@ -0,0 +1,45 @@
+import random 
+
+subjects = [
+    "The cat",
+    "A dog",
+    "The teacher",
+    "A student",
+    "The chef",
+    "The scientist",
+    "A musician",
+    "The artist",
+    "Auto Rickshaw Driver"]
+
+actions= [
+    "jumps over",
+    "runs around", 
+    "sings to",
+    "paints",
+    "teaches",
+    "cooks for",
+    "plays with",
+    "drives",
+    "explains to",
+    "dances with"
+    "cry with"
+]
+places = [
+    "the park",
+    "the school",
+    "the kitchen",
+    "the stage",
+    "the lab",
+    "the street",
+    "the gallery",
+    "the concert hall",
+    "the market",
+    "the beach"
+]
+
+while True:
+    subjects = randome.choice(subjects)
+    actions = random.choice(actions)
+    places = random.choice(places)
+
+    headline = f
